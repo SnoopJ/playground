eeb83e5583524483a22ab25474562475415db24a
James Gerity <snoopjedi@gmail.com>
2022-12-30 13:22:17 -0500

    Add day4 part 2 solution

--- adventofcode/2022/day4/day4.py
+++ adventofcode/2022/day4/day4.py
@@ -12,30 +12,41 @@ def sections(line: str) -> tuple[Assignment, Assignment]:
     return tuple(tuple(int(num) for num in assmnt.split('-')) for assmnt in assmnts)
 
 
-def full_overlap(a: Assignment, b: Assignment) -> bool:
+def overlap(a: Assignment, b: Assignment, full: bool = False) -> bool:
     # NOTE: assuming that a,b are sorted here, i.e. a1 <= a2, b1 <= b2
     a1, a2 = a
     b1, b2 = b
 
-    if ((a1 <= b1 and a2 >= b2) or
-        (b1 <= a1 and b2 >= a2)):
-        return True
+    if full:
+        if ((a1 <= b1 and a2 >= b2) or
+            (b1 <= a1 and b2 >= a2)):
+            return True
     else:
-        return False
+        if ((a1 <= b1 and a2 >= b1) or
+            (b1 <= a1 and b2 >= a1)):
+            return True
+
+    return False
 
 
 def part1(assmnts: list[Assignment]):
     overlaps = 0
     for a, b in assmnts:
-        if full_overlap(a, b):
+        if overlap(a, b, full=True):
             overlaps += 1
 #             print(f"Full overlap: {a}, {b}")
 
     return overlaps
 
 
-def part2():
-    pass
+def part2(assmnts: list[Assignment]):
+    overlaps = 0
+    for a, b in assmnts:
+        if overlap(a, b, full=False):
+            overlaps += 1
+#             print(f"Partial overlap: {a}, {b}")
+
+    return overlaps
 
 
 @click.command()
@@ -44,8 +55,11 @@ def main(input):
     with open(input, "r") as f:
         assignments = [sections(line) for line in f]
 
-    num_overlaps = part1(assignments)
-    print(f"Part 1: {num_overlaps} full overlaps")
+    num_full_overlaps = part1(assignments)
+    print(f"Part 1: {num_full_overlaps} full overlaps")
+
+    num_overlaps = part2(assignments)
+    print(f"Part 2: {num_overlaps} overlaps")
 
 if __name__ == '__main__':
     main()
