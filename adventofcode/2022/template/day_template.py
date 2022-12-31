0afd9ec31b85160629af8e4175a85c5bebc1f1a4
James Gerity <snoopjedi@gmail.com>
2022-12-31 15:46:49 -0500

    Add debug flag to template

--- adventofcode/2022/template/day_template.py
+++ adventofcode/2022/template/day_template.py
@@ -1,25 +1,24 @@
 import click
 
 
-def part1():
+def part1(debug: bool = False):
     pass
 
-
-def part2():
+def part2(debug: bool = False):
     pass
 
 
 @click.command()
 @click.option('--input', required=True, help="Input file for this day")
-def main(input):
+@click.option('--debug', is_flag=True, default=False, help="Enable debug output")
+def main(input, debug):
     with open(input, "r") as f:
         lines = [line.strip() for line in f]
 
-
-    # ans1 = part1()
+    # ans1 = part1(debug=debug)
     # print(f"Part 1: {ans1}")
 
-    # ans2 = part2()
+    # ans2 = part2(debug=debug)
     # print(f"Part 2: {ans2}")
 
 if __name__ == '__main__':
