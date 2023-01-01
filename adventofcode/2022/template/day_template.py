df58619ebe94347b2c70a08df329455be905fee4
James Gerity <snoopjedi@gmail.com>
2023-01-01 18:03:22 -0500

    Add logging to AoC template

--- adventofcode/2022/template/day_template.py
+++ adventofcode/2022/template/day_template.py
@@ -1,11 +1,17 @@
+import logging
+import sys
+
 import click
 
 
-def part1(debug: bool = False):
+LOGGER = logging.getLogger(__name__)
+
+
+def part1():
     pass
 
 
-def part2(debug: bool = False):
+def part2():
     pass
 
 
@@ -13,14 +19,19 @@ def part2(debug: bool = False):
 @click.option('--input', required=True, help="Input file for this day")
 @click.option('--debug', is_flag=True, default=False, help="Enable debug output")
 def main(input, debug):
+    logging.basicConfig(
+        level=logging.DEBUG if debug else logging.WARNING,
+        stream=sys.stdout,
+    )
+
     with open(input, "r") as f:
         lines = [line.strip() for line in f]
 
-    # ans1 = part1(debug=debug)
-    # print(f"Part 1: {ans1}")
+#     ans1 = part1()
+#     print(f"Part 1: {ans1}")
 
-    # ans2 = part2(debug=debug)
-    # print(f"Part 2: {ans2}")
+#     ans2 = part2()
+#     print(f"Part 2: {ans2}")
 
 
 if __name__ == '__main__':
