41e540f331a6d0e244cf4edbd7e99a062d048cb4
James Gerity <snoopjedi@gmail.com>
2023-12-03 12:38:35 -0500

    Add day 3 part 1 solution

---
+++ adventofcode/2023/day3/day3.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+import itertools
+import logging
+import re
+import sys
+from dataclasses import dataclass
+from typing import Iterable
+
+import click
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+@dataclass
+class Schematic:
+    rows: list[str]
+
+    UNSYMS = ".0123456789"
+    neighbor_indices = list(itertools.product(range(-1, 2), repeat=2))
+
+    @property
+    def nrows(self) -> int:
+        return len(self.rows)
+
+    @property
+    def ncols(self) -> int:
+        assert len(len(col) == len(self.rows[0]) for col in self.rows[1:]), "Schematic columns are not even-width!"
+        return len(self.rows[0])
+
+    def part_numbers(self) -> Iterable[int]:
+        for ridx, r in enumerate(self.rows):
+            for m in re.finditer(r"\d+", r):
+                cidx = m.start()
+                num = int(m.group())
+                if self.is_part_number(ridx, cidx):
+                    LOGGER.debug("Found part number at (%s, %s): %s", ridx, cidx, num)
+                    yield num
+                else:
+                    LOGGER.debug("Number %s at (%s, %s) is NOT a part number", num, ridx, cidx)
+
+    def is_part_number(self, row: int, col: int) -> bool:
+        col_offset = 0
+        while True:
+            cidx = col + col_offset
+            if cidx >= len(self.rows[0]) or not self.rows[row][cidx].isdigit():
+                break
+
+            # if a character is not an "unsymbol" it is a symbol by definition
+            # insofar as AoC 2023 is doing """definitions""" anyway :|
+            if any(n not in self.UNSYMS for n in self.neighbors(row, cidx)):
+                return True
+
+            col_offset += 1
+
+        return False
+
+    def neighbors(self, row: int, col: int) -> Iterable[str]:
+        assert row >= 0 and col >= 0, "Negative row or column indices not supported"
+
+        for (r_offset, c_offset) in self.neighbor_indices:
+            r = row + r_offset
+            c = col + c_offset
+            if r < 0 or c < 0 or r >= len(self.rows) or c >= len(self.rows[0]):
+#                 LOGGER.debug("Skipping off-edge neighbor (%s, %s) of (%s, %s)", r, c, row, col)
+                continue
+
+            yield self.rows[r][c]
+
+
+@click.command()
+@click.option('--input', required=True, help="Input file for this day")
+@click.option('--debug', is_flag=True, default=False, help="Enable debug output")
+def main(input, debug):
+    logging.basicConfig(
+        level=logging.DEBUG if debug else logging.WARNING,
+        stream=sys.stdout,
+    )
+
+    with open(input, "r") as f:
+        schematic = Schematic([line.strip() for line in f])
+
+    ans1 = sum(schematic.part_numbers())
+    print(f"Part 1: {ans1}")
+
+#     ans2 = another_miracle_occurs(lines)
+#     print(f"Part 2: {ans2}")
+
+
+if __name__ == '__main__':
+    main()
