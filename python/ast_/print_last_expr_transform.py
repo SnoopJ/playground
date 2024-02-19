46eae8a5e7228c072f0d0788d1c72aa3ea75a500
James Gerity <snoopjedi@gmail.com>
2024-02-19 00:29:09 -0500

    Add AST last-expr print transformation

---
+++ python/ast_/print_last_expr_transform.py
@@ -0,0 +1,64 @@
+import ast
+
+
+class LastExprPrintTransformer(ast.NodeVisitor):
+    """A helper class for finding `print()` and storing the last `Expr` in an AST"""
+    def __init__(self):
+        self.print_seen = False
+
+    def visit_Expr(self, node):
+        self.lastexpr = node
+
+        if isinstance(node.value, ast.Call) and node.value.func.id == "print":
+            self.print_seen = True
+
+
+def wrap_node_print(node: ast.Expr) -> None:
+    """Helper to transform an `Expr` into the equivalent form wrapped in `print()`"""
+
+    val = node.value
+    node.value = ast.Call(
+        lineno=node.lineno,
+        col_offset=node.lineno,
+        end_lineno=node.end_lineno,
+        end_col_offset=node.end_col_offset,
+        func=ast.Name(
+            lineno=node.lineno,
+            col_offset=-1,
+            end_lineno=node.end_lineno,
+            end_col_offset=-1,
+            id='print',
+            ctx=ast.Load(),
+        ),
+        args=[val],
+        keywords=[],
+    )
+
+
+
+def maybe_print_last(src: str) -> str:
+    """Wrap a `print()` around the last expression in the given source code, if no other `print()` is present"""
+    module = ast.parse(src)
+
+    lept = LastExprPrintTransformer()
+
+    new_module = lept.visit(module)
+    if not lept.print_seen:
+        wrap_node_print(lept.lastexpr)
+
+    new_src = ast.unparse(module)
+    return new_src
+
+
+if __name__ == "__main__":
+    TEST_PROGRAMS = [
+        "1 + 1",
+        "1 + 1\nprint(42)",
+    ]
+
+    for src in TEST_PROGRAMS:
+        print("Input source:\n=============")
+        print(src)
+
+        print("\nOutput source:\n=============")
+        print(maybe_print_last(src))
