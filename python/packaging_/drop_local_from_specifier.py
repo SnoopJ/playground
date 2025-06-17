409f601049551165daa84519c913bca21c592655
James Gerity <snoopjedi@gmail.com>
2025-06-17 17:40:15 -0400

    Add example of dropping the local version identifier from a Requirement

---
+++ python/packaging_/drop_local_from_specifier.py
@@ -0,0 +1,29 @@
+from packaging.version import Version
+from packaging.requirements import Requirement
+from packaging.specifiers import Specifier, SpecifierSet
+
+
+def drop_local_from_req(req: Requirement) -> Requirement:
+    """
+    Drop `local` field from any version specifiers in the given `Requirement`
+
+    NOTE: handles multiple specifiers, extras, environment markers, but the local
+    version identifier is dropped from *ALL* specifiers present
+    """
+    result = Requirement(str(req))  # take a copy
+    newspecs = []
+    for spec in req.specifier:
+        newver = Version(spec.version).public
+        ns = Specifier(spec.operator + newver)
+        newspecs.append(ns)
+
+    result.specifier = SpecifierSet(','.join(map(str, newspecs)))
+
+    return result
+
+
+if __name__ == "__main__":
+    req = Requirement('pkg[ex1, ex2]==1.2.3.dev+local, <2; sys.platform == "Windows" or sys.platform == "BeOS"')
+    new_req = drop_local_from_req(req)
+    print(f"Before: {str(req)!r}")
+    print(f"After:  {str(new_req)!r}")
